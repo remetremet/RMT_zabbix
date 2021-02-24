f9d025569d63374655793bd3902740261b68a15d
Martin Červený <79605337+remetremet@users.noreply.github.com>
2021-02-24 22:22:19 +0100

    Create if6_traffic.sh

---
+++ if6_traffic.sh
@@ -0,0 +1,61 @@
+#!/usr/local/bin/bash
+
+ZBXPATH=$( dirname "$(realpath $0)" )
+. ${ZBXPATH}/_database.sh
+
+TEMPFILE="${ZBXDIR}/if6_traffic"
+
+IPV4=0
+IPV4IN=0
+IPV4OUT=0
+IPV6=0
+IPV6IN=0
+IPV6OUT=0
+
+IPV4A=`/sbin/ipfw -a list | grep "count" | grep "^13000 " | /usr/bin/awk '{print $3}' `; IPV4A=${IPV4A:-0}
+echo "${IPV4A}" > ${TEMPFILE}.ipv4_in1
+IPV4IN=$(( $IPV4IN + $IPV4A ))
+IPV4A=`/sbin/ipfw -a list | grep "count" | grep "^13100 " | /usr/bin/awk '{print $3}' `; IPV4A=${IPV4A:-0}
+echo "${IPV4A}" > ${TEMPFILE}.ipv4_in2
+IPV4IN=$(( $IPV4IN + $IPV4A ))
+IPV4A=`/sbin/ipfw -a list | grep "count" | grep "^13200 " | /usr/bin/awk '{print $3}' `; IPV4A=${IPV4A:-0}
+echo "${IPV4A}" > ${TEMPFILE}.ipv4_in3
+IPV4IN=$(( $IPV4IN + $IPV4A ))
+echo "${IPV4IN}" > ${TEMPFILE}.ipv4_in
+IPV4B=`/sbin/ipfw -a list | grep "count" | grep "^15000 " | /usr/bin/awk '{print $3}' `; IPV4B=${IPV4B:-0}
+echo "${IPV4B}" > ${TEMPFILE}.ipv4_out1
+IPV4OUT=$(( $IPV4OUT + $IPV4B ))
+IPV4B=`/sbin/ipfw -a list | grep "count" | grep "^15100 " | /usr/bin/awk '{print $3}' `; IPV4B=${IPV4B:-0}
+echo "${IPV4B}" > ${TEMPFILE}.ipv4_out2
+IPV4OUT=$(( $IPV4OUT + $IPV4B ))
+IPV4B=`/sbin/ipfw -a list | grep "count" | grep "^15200 " | /usr/bin/awk '{print $3}' `; IPV4B=${IPV4B:-0}
+echo "${IPV4B}" > ${TEMPFILE}.ipv4_out3
+IPV4OUT=$(( $IPV4OUT + $IPV4B ))
+echo "${IPV4OUT}" > ${TEMPFILE}.ipv4_out
+IPV4=$(( $IPV4 + $IPV4IN + $IPV4OUT ))
+echo "${IPV4}" > ${TEMPFILE}.ipv4
+
+IPV6A=`/sbin/ipfw -a list | grep "count" | grep "^09000 " | /usr/bin/awk '{print $3}' `; IPV6A=${IPV6A:-0}
+echo "${IPV6A}" > ${TEMPFILE}.ipv6_in1
+IPV6IN=$(( $IPV6IN + $IPV6A ))
+IPV6A=`/sbin/ipfw -a list | grep "count" | grep "^09100 " | /usr/bin/awk '{print $3}' `; IPV6A=${IPV6A:-0}
+echo "${IPV6A}" > ${TEMPFILE}.ipv6_in2
+IPV6IN=$(( $IPV6IN + $IPV6A ))
+IPV6A=`/sbin/ipfw -a list | grep "count" | grep "^09200 " | /usr/bin/awk '{print $3}' `; IPV6A=${IPV6A:-0}
+echo "${IPV6A}" > ${TEMPFILE}.ipv6_in3
+IPV6IN=$(( $IPV6IN + $IPV6A ))
+echo "${IPV6IN}" > ${TEMPFILE}.ipv6_in
+IPV6B=`/sbin/ipfw -a list | grep "count" | grep "^11000 " | /usr/bin/awk '{print $3}' `; IPV6B=${IPV6B:-0}
+echo "${IPV6B}" > ${TEMPFILE}.ipv6_out1
+IPV6OUT=$(( $IPV6OUT + $IPV6B ))
+IPV6B=`/sbin/ipfw -a list | grep "count" | grep "^11100 " | /usr/bin/awk '{print $3}' `; IPV6B=${IPV6B:-0}
+echo "${IPV6B}" > ${TEMPFILE}.ipv6_out2
+IPV6OUT=$(( $IPV6OUT + $IPV6B ))
+IPV6B=`/sbin/ipfw -a list | grep "count" | grep "^11200 " | /usr/bin/awk '{print $3}' `; IPV6B=${IPV6B:-0}
+echo "${IPV6B}" > ${TEMPFILE}.ipv6_out3
+IPV6OUT=$(( $IPV6OUT + $IPV6B ))
+echo "${IPV6OUT}" > ${TEMPFILE}.ipv6_out
+IPV6=$(( $IPV6 + $IPV6IN + $IPV6OUT ))
+echo "${IPV6}" > ${TEMPFILE}.ipv6
+
+chmod 666 ${TEMPFILE}.ip*
