49ab5a7c670d51a990327307c1c93b1bad24271d
Martin Červený <79605337+remetremet@users.noreply.github.com>
2021-02-26 02:13:31 +0100

    Update _update.sh

--- _update.sh
+++ _update.sh
@@ -2,10 +2,11 @@
 ZBXPATH=$( dirname "$(realpath $0)" )
 . "${ZBXPATH}/_database.sh"
 
-#file_to_update="_cron.sh _database.sh freebsd-update.sh if6_traffic.sh mfi_get.sh network_discovery.sh pkg.sh rping.sh speedtest.sh smartctl.sh"
-#github_file="_cron.sh"
-#FILE="https://api.github.com/repos/${github_owner}/${github_repo}/contents/${github_file}"
-#/usr/local/bin/curl -v --header "Authorization: token ${github_token}" --header "Accept: application/vnd.github.v3.raw" --remote-name --location $FILE
+# Setup
+#git config --global pull.rebase false
+#git config --global user.email "remet@remet.cz"
+#git config --global credential.helper store
+#git clone https://github.com/remetremet/RMT_zabbix.git /var/zabbix/github
 
 # Synchronize scripts from github to local repo
 cd ${ZBXPATH}/github
