a0da279bed2b73664ab0c14bf706e889fc249a0f
Martin Červený <79605337+remetremet@users.noreply.github.com>
2021-02-26 02:02:16 +0100

    Update _update.sh

--- _update.sh
+++ _update.sh
@@ -2,10 +2,12 @@
 ZBXPATH=$( dirname "$(realpath $0)" )
 . "${ZBXPATH}/_database.sh"
 
-file_to_update="_cron.sh _database.sh freebsd-update.sh if6_traffic.sh mfi_get.sh network_discovery.sh pkg.sh rping.sh speedtest.sh smartctl.sh"
+#file_to_update="_cron.sh _database.sh freebsd-update.sh if6_traffic.sh mfi_get.sh network_discovery.sh pkg.sh rping.sh speedtest.sh smartctl.sh"
+#github_file="_cron.sh"
+#FILE="https://api.github.com/repos/${github_owner}/${github_repo}/contents/${github_file}"
+#/usr/local/bin/curl -v --header "Authorization: token ${github_token}" --header "Accept: application/vnd.github.v3.raw" --remote-name --location $FILE
 
-github_file="_cron.sh"
-FILE="https://api.github.com/repos/${github_owner}/${github_repo}/contents/${github_file}"
-/usr/local/bin/curl -v --header "Authorization: token ${github_token}" --header "Accept: application/vnd.github.v3.raw" --remote-name --location $FILE
-
-chmod 775 "${ZBXPATH}/*.sh"
+cd ${ZBXPATH}/github
+git pull origin master
+cp -R ${ZBXPATH}/github/*.sh ${ZBXPATH}/
+chmod 775 ${ZBXPATH}/*.sh
