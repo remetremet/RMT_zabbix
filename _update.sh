03776e888ade260c9a61744cba18aabc1b55feab
Martin Červený <79605337+remetremet@users.noreply.github.com>
2021-02-26 02:39:50 +0100

    Update _update.sh

--- _update.sh
+++ _update.sh
@@ -2,12 +2,6 @@
 ZBXPATH=$( dirname "$(realpath $0)" )
 . "${ZBXPATH}/_database.sh"
 
-# Setup
-#git config --global pull.rebase false
-#git config --global user.email "remet@remet.cz"
-#git config --global credential.helper store
-#git clone https://github.com/remetremet/RMT_zabbix.git /var/zabbix/github
-
 # Synchronize scripts from github to local repo
 cd ${ZBXPATH}/github
 git pull origin master
