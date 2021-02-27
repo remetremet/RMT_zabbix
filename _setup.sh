07c158d8fd5fcc210ea7d0d9fd35e3d0de104cf1
Martin Červený <79605337+remetremet@users.noreply.github.com>
2021-02-27 09:34:50 +0100

    Update and rename _create_local_git.sh to _setup.sh

--- _setup.sh
+++ _setup.sh
@@ -1,6 +1,16 @@
 #!/usr/local/bin/bash
 
-# Setup
+# Setup directories
+if [ ! -d /var/zabbix ]; then
+ mkdir /var/zabbix
+ chmod 777 /var/zabbix
+fi
+if [ ! -d /var/zabbix/temp ]; then
+ mkdir /var/zabbix/temp
+ chmod 777 /var/zabbix/temp
+fi
+
+# Setup GITHUB / local repo
 git config --global pull.rebase false
 git config --global user.email "remet@remet.cz"
 git config --global credential.helper store
