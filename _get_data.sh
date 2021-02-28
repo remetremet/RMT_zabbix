4e0c04b315297ba40648a552fc1ae842c839e07c
Martin Červený <79605337+remetremet@users.noreply.github.com>
2021-02-28 23:18:14 +0100

    Update _get_data.sh

--- _get_data.sh
+++ _get_data.sh
@@ -11,10 +11,24 @@ fi
 FUNCTION=$1
 case ${FUNCTION} in
         pkg)
-         echo "1"
+         cat "${DATAPATH}/pkg"
         ;;
-        freebsd_update)
-         echo "2"
+        freebsd_update.*)
+         INDEX="${FUNCTION:15}"
+         case ${INDEX} in
+                 latest)
+                  cat "${DATAPATH}/freebsd_update.latest"
+                 ;;
+                 running)
+                  cat "${DATAPATH}/freebsd_update.running"
+                 ;;
+                 EOL)
+                  cat "${DATAPATH}/freebsd_update.EOL"
+                 ;;
+                 *)
+                  echo 0
+                 ;;
+         esac
         ;;
         network_discovery)
          cat "${DATAPATH}/network_discovery"
@@ -23,21 +37,67 @@ case ${FUNCTION} in
          INDEX="${FUNCTION:12}"
          cat "${DATAPATH}/network_discovery.${INDEX}.mac"
         ;;
+        ipfw_traffic.*)
+         INDEX="${FUNCTION:13}"
+         case ${INDEX} in
+                 ipv4)
+                  cat "${DATAPATH}/ipfw_traffic.ipv4"
+                 ;;
+                 ipv6)
+                  cat "${DATAPATH}/ipfw_traffic.ipv6"
+                 ;;
+                 ipv4_in)
+                  cat "${DATAPATH}/ipfw_traffic.ipv4_in"
+                 ;;
+                 ipv6_in)
+                  cat "${DATAPATH}/ipfw_traffic.ipv6_in"
+                 ;;
+                 ipv4_out)
+                  cat "${DATAPATH}/ipfw_traffic.ipv4_out"
+                 ;;
+                 ipv6_out)
+                  cat "${DATAPATH}/ipfw_traffic.ipv6_out"
+                 ;;
+                 *)
+                  echo 0
+                 ;;
+         esac
+        ;;
+        rping_discovery)
+         cat "${DATAPATH}/rping_discovery"
+        ;;
+        rping.*)
+         INDEX="${FUNCTION:6}"
+         cat "${DATAPATH}/rping_${INDEX}"
+        ;;
+        rpl.*)
+         INDEX="${FUNCTION:4}"
+         cat "${DATAPATH}/rpl_${INDEX}"
+        ;;
+        smartctl_discovery)
+         cat "${DATAPATH}/smartctl_discovery"
+        ;;
+        smartctl.*)
+         INDEX="${FUNCTION:9}"
+         cat "${DATAPATH}/smartctl.${INDEX}"
+        ;;
         speedtest_discovery)
-         echo "4"
+         cat "${DATAPATH}/speedtest_discovery"
         ;;
         speedtest.*.extip)
-         echo "5"
+         INDEX="${FUNCTION:10:1}"
+         cat "${DATAPATH}/speedtest_${INDEX}.extip"
         ;;
         speedtest.*.down)
-         echo "6"
+         INDEX="${FUNCTION:10:1}"
+         cat "${DATAPATH}/speedtest_${INDEX}.down"
         ;;
         speedtest.*.up)
-         echo "7"
+         INDEX="${FUNCTION:10:1}"
+         cat "${DATAPATH}/speedtest_${INDEX}.up"
         ;;
 # other/unknown function request
         *)
          echo "0"
-         exit
         ;;
 esac
