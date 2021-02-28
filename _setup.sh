ae9ca61eeb8548ad4ccb647de7765677a06904f0
Martin Červený <79605337+remetremet@users.noreply.github.com>
2021-02-28 14:21:05 +0100

    Update _setup.sh

--- _setup.sh
+++ _setup.sh
@@ -1,38 +1,38 @@
 #!/usr/local/bin/bash
 
 # Define base directory
-ZBXDIR="/var/zabbix"
+ZBXPATH="/var/zabbix"
 
 # Software requirements
 pkg -y install git fping curl smartmontools
 
 # Setup directories
-if [ ! -d "${ZBXDIR}" ]; then
- mkdir "${ZBXDIR}"
- chmod 777 "${ZBXDIR}"
+if [ ! -d "${ZBXPATH}" ]; then
+ mkdir "${ZBXPATH}"
+ chmod 777 "${ZBXPATH}"
 fi
-if [ ! -d "${ZBXDIR}/temp" ]; then
- mkdir "${ZBXDIR}/temp"
- chmod 777 "${ZBXDIR}/temp"
+if [ ! -d "${ZBXPATH}/temp" ]; then
+ mkdir "${ZBXPATH}/temp"
+ chmod 777 "${ZBXPATH}/temp"
 fi
-if [ ! -d "${ZBXDIR}/data" ]; then
- mkdir "${ZBXDIR}/data"
- chmod 777 "${ZBXDIR}/data"
+if [ ! -d "${ZBXPATH}/data" ]; then
+ mkdir "${ZBXPATH}/data"
+ chmod 777 "${ZBXPATH}/data"
 fi
-if [ ! -d "${ZBXDIR}/scripts" ]; then
- mkdir "${ZBXDIR}/scripts"
- chmod 777 "${ZBXDIR}/scripts"
+if [ ! -d "${ZBXPATH}/scripts" ]; then
+ mkdir "${ZBXPATH}/scripts"
+ chmod 777 "${ZBXPATH}/scripts"
 fi
 
 # Setup GITHUB / local repo
 git config --global pull.rebase false
 git config --global user.email "remet@remet.cz"
 git config --global credential.helper store
-git clone https://github.com/remetremet/RMT_zabbix.git ${ZBXDIR}/github
+git clone https://github.com/remetremet/RMT_zabbix.git ${ZBXPATH}/github
 
 # Copy scripts to working directory
-cp -R ${ZBXDIR}/github/_*.sh ${ZBXDIR}/
-chmod 755 ${ZBXDIR}/_*.sh
-cp -R ${ZBXDIR}/github/*.sh ${ZBXDIR}/scripts/
-rm -f ${ZBXDIR}/scripts/_*.sh
-chmod 755 ${ZBXDIR}/scripts/*.sh
+cp -R ${ZBXPATH}/github/_*.sh ${ZBXPATH}/
+chmod 755 ${ZBXPATH}/_*.sh
+cp -R ${ZBXPATH}/github/*.sh ${ZBXPATH}/scripts/
+rm -f ${ZBXPATH}/scripts/_*.sh
+chmod 755 ${ZBXPATH}/scripts/*.sh
