055ffaaed59347b3f615d429eff1c4482941baf5
Martin Červený <79605337+remetremet@users.noreply.github.com>
2021-02-28 20:17:58 +0100

    Update _get_data.sh

--- _get_data.sh
+++ _get_data.sh
@@ -10,3 +10,32 @@ fi
 
 echo "P1: $1"
 echo "P2: $2"
+
+case ${FUNCTION} in
+        pkg)
+         echo "1"
+        ;;
+        freebsd_update)
+         echo "2"
+        ;;
+        network.discovery)
+         echo "3"
+        ;;
+        speedtest.discovery)
+         echo "4"
+        ;;
+        speedtest.extip)
+         echo "5"
+        ;;
+        speedtest.*.down)
+         echo "6"
+        ;;
+        speedtest.*.up)
+         echo "7"
+        ;;
+# other/unknown function request
+        *)
+         echo "0"
+         exit
+        ;;
+esac
