71be971583b23149e2544bb3b084248ceb686615
Martin Červený <79605337+remetremet@users.noreply.github.com>
2021-02-28 14:21:21 +0100

    Update _update.sh

--- _update.sh
+++ _update.sh
@@ -2,6 +2,23 @@
 ZBXPATH=$( dirname "$(realpath $0)" )
 . "${ZBXPATH}/_database.sh"
 
+if [ ! -d "${ZBXPATH}" ]; then
+ mkdir "${ZBXPATH}"
+ chmod 777 "${ZBXPATH}"
+fi
+if [ ! -d "${ZBXPATH}/temp" ]; then
+ mkdir "${ZBXPATH}/temp"
+ chmod 777 "${ZBXPATH}/temp"
+fi
+if [ ! -d "${ZBXPATH}/data" ]; then
+ mkdir "${ZBXPATH}/data"
+ chmod 777 "${ZBXPATH}/data"
+fi
+if [ ! -d "${ZBXPATH}/scripts" ]; then
+ mkdir "${ZBXPATH}/scripts"
+ chmod 777 "${ZBXPATH}/scripts"
+fi
+
 # Synchronize scripts from github to local repo
 cd ${ZBXPATH}/github
 git pull origin master
