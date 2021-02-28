350dd6fff694b85b9b57ecea46f1c0023724e9ce
Martin Červený <79605337+remetremet@users.noreply.github.com>
2021-02-28 16:27:23 +0100

    Update _update.sh

--- _update.sh
+++ _update.sh
@@ -1,7 +1,12 @@
 #!/usr/local/bin/bash -
 ZBXPATH=$( dirname "$(realpath $0)" )
+. "${ZBXPATH}/_config.sh"
 . "${ZBXPATH}/_database.sh"
 SCRIPTSPATH="${ZBXPATH}/scripts"
+
+if [ x"${AUTOUPDATE}" == x"no" ]; then
+ exit;
+fi
 
 if [ ! -d "${ZBXPATH}" ]; then
  mkdir "${ZBXPATH}"
@@ -27,6 +32,5 @@ git pull origin master
 # Copy scripts to working directory
 cp -R ${ZBXPATH}/github/_*.sh ${ZBXPATH}/
 chmod 755 ${ZBXPATH}/_*.sh
-cp -R ${ZBXPATH}/github/*.sh ${SCRIPTSPATH}/
-rm -f ${SCRIPTSPATH}/_*.sh
+cp -R ${ZBXPATH}/github/scripts/*.sh ${SCRIPTSPATH}/
 chmod 755 ${SCRIPTSPATH}/*.sh
