37af1baa3b7e1558eebe16ffc0fc43ba35dbc3f2
Martin Červený <79605337+remetremet@users.noreply.github.com>
2021-02-28 14:18:50 +0100

    Update _setup.sh

--- _setup.sh
+++ _setup.sh
@@ -15,6 +15,14 @@ if [ ! -d "${ZBXDIR}/temp" ]; then
  mkdir "${ZBXDIR}/temp"
  chmod 777 "${ZBXDIR}/temp"
 fi
+if [ ! -d "${ZBXDIR}/data" ]; then
+ mkdir "${ZBXDIR}/data"
+ chmod 777 "${ZBXDIR}/data"
+fi
+if [ ! -d "${ZBXDIR}/scripts" ]; then
+ mkdir "${ZBXDIR}/scripts"
+ chmod 777 "${ZBXDIR}/scripts"
+fi
 
 # Setup GITHUB / local repo
 git config --global pull.rebase false
@@ -23,5 +31,8 @@ git config --global credential.helper store
 git clone https://github.com/remetremet/RMT_zabbix.git ${ZBXDIR}/github
 
 # Copy scripts to working directory
-cp -R ${ZBXDIR}/github/*.sh ${ZBXDIR}/
-chmod 755 ${ZBXDIR}/*.sh
+cp -R ${ZBXDIR}/github/_*.sh ${ZBXDIR}/
+chmod 755 ${ZBXDIR}/_*.sh
+cp -R ${ZBXDIR}/github/*.sh ${ZBXDIR}/scripts/
+rm -f ${ZBXDIR}/scripts/_*.sh
+chmod 755 ${ZBXDIR}/scripts/*.sh
