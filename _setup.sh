e59987c20b729fc03e9b6c59dfdbb94b7f6b85e7
Martin Červený <79605337+remetremet@users.noreply.github.com>
2021-03-01 11:55:21 +0100

    Update _setup.sh

--- _setup.sh
+++ _setup.sh
@@ -36,3 +36,26 @@ cp -R ${ZBXPATH}/github/_*.sh ${ZBXPATH}/
 chmod 755 ${ZBXPATH}/_*.sh
 cp -R ${ZBXPATH}/github/scripts/*.sh ${SCRIPTSPATH}/
 chmod 755 ${SCRIPTSPATH}/*.sh
+
+# Modify Zabbix Agent configuration and restart Zabbix Agent
+if [ -d "/usr/local/etc/zabbix43/zabbix_agentd.conf.d" ]; then
+ ZBXCONFDIR="/usr/local/etc/zabbix43/zabbix_agentd.conf.d"
+fi
+if [ -d "/usr/local/etc/zabbix44/zabbix_agentd.conf.d" ]; then
+ ZBXCONFDIR="/usr/local/etc/zabbix44/zabbix_agentd.conf.d"
+fi
+if [ -d "/usr/local/etc/zabbix50/zabbix_agentd.conf.d" ]; then
+ ZBXCONFDIR="/usr/local/etc/zabbix50/zabbix_agentd.conf.d"
+fi
+if [ -d "/usr/local/etc/zabbix51/zabbix_agentd.conf.d" ]; then
+ ZBXCONFDIR="/usr/local/etc/zabbix51/zabbix_agentd.conf.d"
+fi
+if [ -d "/usr/local/etc/zabbix52/zabbix_agentd.conf.d" ]; then
+ ZBXCONFDIR="/usr/local/etc/zabbix52/zabbix_agentd.conf.d"
+fi
+if [ -d "/usr/local/etc/zabbix/zabbix_agentd.conf.d" ]; then
+ ZBXCONFDIR="/usr/local/etc/zabbix/zabbix_agentd.conf.d"
+fi
+cp -f ${ZBXPATH}/github/templates/userparameter_rmtzabbix.conf ${ZACONF}/userparameter_rmtzabbix.conf
+/usr/local/etc/rc.d/zabbix_agentd restart
+
