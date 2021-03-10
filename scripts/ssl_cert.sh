46c85b586993a2f10d0d5a5e7643bb65721a9175
Martin Červený <79605337+remetremet@users.noreply.github.com>
2021-03-10 20:03:17 +0100

    Update ssl_cert.sh

--- scripts/ssl_cert.sh
+++ scripts/ssl_cert.sh
@@ -1,17 +1,26 @@
-#! /bin/sh
-#------------------------------------------------------------
-# zext_ssl_cert.sh
-# Script checks for number of days until certificate expires or the issuing authority
-# depending on switch passed on command line.
-#
-#Based on script from aperto.fr (http://aperto.fr/cms/en/blog/15-blog-en/15-ssl-certificate-expiration-monitoring-with-zabbix.html)
-#with additions by racooper@tamu.edu
-#Hivlaher additions for FreeBSD. Made the changes needed for the 
-#date command so it works with FreeBSD date:)
-#Also added a sed command to cut the " GMT" on the $end_date so
-#it doesnt give an ignore error. So if your timezone is not GMT
-#you may have to condider the Time difference :)
-#------------------------------------------------------------
+#!/usr/local/bin/bash -
+ZBXPATH=$( dirname "$(realpath $0)" )
+if [[ -e "${ZBXPATH}/../_config.sh" ]]; then
+ ZBXPATH=$( dirname "${ZBXPATH}" )
+else
+ ZBXPATH="${ZBXPATH}"
+fi
+if [[ ! -e "${ZBXPATH}/_config.sh" ]]; then
+ exit;
+fi
+. ${ZBXPATH}/_config.sh
+if [[ ! -e "${ZBXPATH}/_database.sh" ]]; then
+ exit;
+fi
+. ${ZBXPATH}/_database.sh
+
+if [ x"${SSL_ENABLE}" == x"no" ]; then
+ exit;
+fi
+
+TEMPFILE="${TEMPPATH}/zabbix_ssl"
+SEMAPHOREFILE="${TEMPPATH}/.zabbix_ssl"
+ZBXFILE="${DATAPATH}/ssl"
 
 f=$1
 host=$2
@@ -27,28 +36,9 @@ fi
 if [ -n "${proto}" ]; then
   starttls="-starttls ${proto}"
 fi
-case ${f} in
-  -d)
-     end_date=`openssl s_client -servername ${servername} -host ${host} -port ${port} -showcerts ${starttls} -prexit </dev/null 2>/dev/null | sed -n '/BEGIN CERTIFICATE/,/END CERT/p' | openssl x509 -text 2>/dev/null | sed -n 's/ *Not After : *//p' | sed 's/ GMT//g'`
-     if [ -n "${end_date}" ]; then
-#       end_date_seconds=`date '+%s' --date "${end_date}"`
-       end_date_seconds=`date -j -f "%b %d %T %Y" "${end_date}" "+%s"`
-#       echo ${end_date}
-       now_seconds=`date '+%s'`
-#       echo ${now_seconds}
-       echo $(((${end_date_seconds}-${now_seconds})/86400))
-     fi
-  ;;
-  -i)
-     issue_dn=`openssl s_client -servername ${servername} -host ${host} -port ${port} -showcerts ${starttls} -prexit </dev/null 2>/dev/null | sed -n '/BEGIN CERTIFICATE/,/END CERT/p' | openssl x509 -text 2>/dev/null | sed -n 's/ *Issuer: *//p'`
-     if [ -n "${issue_dn}" ]; then
-       issuer=`echo ${issue_dn} | sed -n 's/.*CN=*//p'`
-       echo ${issuer}
-     fi
-  ;;
-  *)
-    echo "usage: $0 [-i|-d] hostname port sni"
-    echo "    -i Show Issuer"
-    echo "    -d Show valid days remaining"
-  ;;
-esac
+end_date=`openssl s_client -servername ${servername} -host ${host} -port ${port} -showcerts ${starttls} -prexit </dev/null 2>/dev/null | sed -n '/BEGIN CERTIFICATE/,/END CERT/p' | openssl x509 -text 2>/dev/null | sed -n 's/ *Not After : *//p' | sed 's/ GMT//g'`
+if [ -n "${end_date}" ]; then
+ end_date_seconds=`date -j -f "%b %d %T %Y" "${end_date}" "+%s"`
+ now_seconds=`date '+%s'`
+ echo $(((${end_date_seconds}-${now_seconds})/86400))
+fi
